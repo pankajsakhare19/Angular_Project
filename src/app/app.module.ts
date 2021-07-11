89c26dfb4dde729bb9cf9de07d9d5dbde111f5ee
pankajsakhare19 <pankajsakhare91@gmail.com>
2021-07-11 21:12:36 +0530

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,15 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LeaderService } from './services/leader.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +36,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -43,9 +48,13 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatDialogModule,
     AppRoutingModule
   ],
-  providers: [DishService, PromotionService],
+  entryComponents: [
+    LoginComponent
+],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
