b73fccd699ad6267fa32c8dd65db3634e54c36ed
pankajsakhare19 <pankajsakhare91@gmail.com>
2021-07-12 12:58:58 +0530

    Template Forms Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,10 @@ import { PromotionService } from './services/promotion.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -49,7 +53,11 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatButtonModule,
     MatDialogModule,
-    AppRoutingModule
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    AppRoutingModule,
+    FormsModule
   ],
   entryComponents: [
     LoginComponent
